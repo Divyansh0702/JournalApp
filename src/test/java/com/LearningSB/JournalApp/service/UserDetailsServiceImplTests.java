f0f52bec3a8454adf0fcbd72722f03cc6b1cf82f
Divyansh0702 <divyansh07020@gmail.com>
2026-07-14 14:03:57 +0530

    Mockito

---
+++ src/test/java/com/LearningSB/JournalApp/service/UserDetailsServiceImplTests.java
@@ -0,0 +1,42 @@
+package com.LearningSB.JournalApp.service;
+
+import com.LearningSB.JournalApp.entity.User;
+import com.LearningSB.JournalApp.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+
+public class UserDetailsServiceImplTests {
+
+    @InjectMocks
+    private  UserDetailsServiceImpl userDetailsService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @BeforeEach
+    void setUp(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void loadUserByUsernameTest(){
+        when(userRepository.findByUserName(ArgumentMatchers.anyString()))
+                .thenReturn(User.builder()
+                        .userName("Divyansh")
+                        .password("password")
+                        .roles(new ArrayList<>())
+                        .build());
+        UserDetails user = userDetailsService.loadUserByUsername("Divyansh");
+        Assertions.assertNotNull(user);
+    }
+}
